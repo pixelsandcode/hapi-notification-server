135236c5c2722c752b2a62eb2fd0ad9b26b0b9b9
Tectual <arash@tectual.com.au>
2015-09-19 18:48:30 +0430

    mock option for testing

--- build/handlers/messages.js
+++ build/handlers/messages.js
@@ -14,11 +14,16 @@
         payload = request.payload;
         message = new Message;
         return message.load(request.payload.template).then(function(template) {
-          _.each(payload.user_keys, function(u) {
-            return Device.find_by_user(u).then(function(device) {
-              return message.deliver(device, payload.data);
+          if (options.config.mock) {
+            message.render(payload.data, 'android');
+            message.render(payload.data, 'iphone');
+          } else {
+            _.each(payload.user_keys, function(u) {
+              return Device.find_by_user(u).then(function(device) {
+                return message.deliver(device, payload.data);
+              });
             });
-          });
+          }
           return reply.success(true);
         }).done();
       }
--- src/handlers/messages.coffee
+++ src/handlers/messages.coffee
@@ -12,9 +12,13 @@ module.exports = (server, options) ->
       payload = request.payload
       message = new Message
       message.load(request.payload.template).then( (template) ->
-        _.each payload.user_keys, (u) ->
-          Device.find_by_user(u).then (device) ->
-            message.deliver( device, payload.data )
+        if options.config.mock
+          message.render payload.data, 'android'
+          message.render payload.data, 'iphone'
+        else
+          _.each payload.user_keys, (u) ->
+            Device.find_by_user(u).then (device) ->
+              message.deliver( device, payload.data )
         reply.success true
       ).done()
   }
